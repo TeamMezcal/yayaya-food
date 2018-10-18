85c4081d5c0cb091894466ad6c894a02b985229b
Jose Bernat <jbf@iesat.com>
2018-10-18 22:26:34 +0200

    foking deploy already

--- models/user.model.js
+++ models/user.model.js
@@ -26,8 +26,8 @@ const userSchema =  new mongoose.Schema({
   location: {
     type: {
       type: String,
-      enum: ['Point'],  
-      default: []
+      enum: ['Point']
+    
     },
     coordinates: {
       type: [Number],
