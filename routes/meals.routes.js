8714829deee0f9d056ef267c91517dcc21855415
Jose Bernat <jbf@iesat.com>
2018-11-10 17:53:43 +0100

    fix: minor fixes in import spelling that prevented compiling

--- routes/meals.routes.js
+++ routes/meals.routes.js
@@ -4,11 +4,11 @@ const meals = require('../controllers/meal.controller');
 const secure = require('../middleware/secure.middleware');
 const user = require('../middleware/user.middleware');
 const reviews = require('../controllers/review.controller');
-const uploader = require('./../configs/multer.config');
+const uploader = require('./../config/multer.config');
 
 router.get('/', meals.list);
 router.post('/', uploader.array('images'), meals.create);
 router.get('/:id', meals.get);
-router.delete('/:id', meals.delete);
+//router.delete('/:id', meals.delete);
 
 module.exports = router;
